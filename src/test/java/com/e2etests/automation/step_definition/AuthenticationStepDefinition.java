e383ba5561c32ab6444252cc368e36779f128bdb
amal901 <amal.aouini.wahbeh@gmail.com>
2022-07-20 23:54:59 +0200

    first commit

---
+++ src/test/java/com/e2etests/automation/step_definition/AuthenticationStepDefinition.java
@@ -0,0 +1,46 @@
+package com.e2etests.automation.step_definition;
+
+import org.junit.Assert;
+
+import com.e2etests.automation.page_object.AuthenticationPage;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class AuthenticationStepDefinition {
+
+	private AuthenticationPage authenticationPage;
+	public AuthenticationStepDefinition() {
+		this.authenticationPage = new AuthenticationPage();
+	}
+
+	@Given("Je me connecte a l application OrangeHRM")
+	public void jeMeConnecteALApplicationOrangeHRM() {
+		authenticationPage.goToURL();
+	}
+
+	@When("Je saisie le username {string}")
+	public void jeSaisieLeUsername(String username) {
+		authenticationPage.fillUsername(username);
+
+	}
+
+	@When("Je saisie le mot le passe {string}")
+	public void jeSaisieLeMotLePasse(String password) {
+		authenticationPage.fillPassword(password);
+
+	}
+
+	@When("Je clique sur le bouton login")
+	public void jeCliqueSurLeBoutonLogin() {
+		authenticationPage.clickOnbtnLogin();
+	}
+
+	@Then("Je me redirige vers la page home {string}")
+	public void jeMeRedirigeVersLaPageHome(String msg) {
+		String message = AuthenticationPage.homePage.getText();
+		Assert.assertTrue(message.contains("msg"));
+	}
+	
+
+}
